c2134b6e81f430fe78d375b61e31e047adc19a29
James Marsh <james.marsh2@pcc.edu>
2022-02-01 15:38:32 -0800

    Initial Commit

---
+++ src/HelloWorld/Java/HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld.Java;
+
+public class HelloWorld
+{
+    public void main()
+    {
+        HelloWorld();
+    }
+    public void HelloWorld()
+    {
+        System.out.println("Hello World!");
+    }
+}
