415137a8743b188f3330e1187e5248244e2aca95
James Marsh <james.marsh2@pcc.edu>
2022-02-02 15:56:11 -0800

    Added a second function to push to GitHub

---
+++ src/FirstProject/Java/HelloWorld.java
@@ -0,0 +1,13 @@
+package FirstProject.Java;
+
+public class HelloWorld
+{
+    public void HelloWorldMessage()
+    {
+        System.out.println("Hello World!");
+    }
+    public void WhatsUpDudes()
+    {
+        System.out.println("Hey, whats up my dudes? Hows it going!");
+    }
+}
