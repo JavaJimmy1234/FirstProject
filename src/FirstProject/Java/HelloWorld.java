add71fec1a1a660651f1085498bb2f687ce7d48f
James Marsh <james.marsh2@pcc.edu>
2022-02-02 16:00:35 -0800

    Added a main method

--- src/FirstProject/Java/HelloWorld.java
+++ src/FirstProject/Java/HelloWorld.java
@@ -2,11 +2,20 @@ package FirstProject.Java;
 
 public class HelloWorld
 {
-    public void HelloWorldMessage()
+    public static void main(int num)
+    {
+        if(num == 1) {
+            HelloWorldMessage();
+        }
+        if(num == 2) {
+            WhatsUpDudes();
+        }
+    }
+    public static void HelloWorldMessage()
     {
         System.out.println("Hello World!");
     }
-    public void WhatsUpDudes()
+    public static void WhatsUpDudes()
     {
         System.out.println("Hey, whats up my dudes? Hows it going!");
     }
